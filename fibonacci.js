e61b13316ce44208ca6193420ad1ed0d13c0d974
Chris K <Chris-SourceCode@outlook.com>
2021-06-21 10:18:35 +0100

    refactor: change fibonacci to be heavy operation
    Fibonacci algorithm changed so it blocks thread of execution for visible amount of time to highlight benefits of using web-workers.

--- fibonacci.js
+++ fibonacci.js
@@ -1,9 +1,7 @@
 function fibonacci(num) {
-  const fibHelper = (num, prev, cur) => {
-    if (num === 0) return prev; // prev was cur @ func invocation
-    return fibHelper(num - 1, cur, prev + cur);
-  };
-  return fibHelper(num, 0, 1);
+  if (num <= 3) return num;
+
+  return fibonacci(num - 1) + fibonacci(num - 2);
 }
 
 export default fibonacci;
